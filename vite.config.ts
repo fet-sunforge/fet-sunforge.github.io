8a2b4480f24a1085c54033c02ac8cc11f332e34b
Richard Wong <ricwtk@gmail.com>
2025-12-02 23:47:57 +0800

    add about page (empty)

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,18 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'about.html'),
+        // fyp: path.resolve(__dirname, 'fyp.html'),
+        // idp: path.resolve(__dirname, 'idp.html'),
+        // sed: path.resolve(__dirname, 'sed.html'),
+        // pmed: path.resolve(__dirname, 'pmed.html'),
+        // dit: path.resolve(__dirname, 'dit.html')
+      }
+    }
   }
 })
