cfbab06a270724eb542377209220f0684d45ccad
Richard Wong <ricwtk@gmail.com>
2025-12-03 23:37:12 +0800

    add about page with global menu

--- src/about/About.vue
+++ src/about/About.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GlobalMenu from "@/components/GlobalMenu.vue";
+</script>
 
 <template>
-  <div>
-    <h1>About Page</h1>
-    <p>This is the about page.</p>
+  <div class="flex flex-col w-screen h-screen bg-slate-700 items-center text-2xl text-white overflow-auto pt-10 pb-10">
+    <GlobalMenu>About SUNForge Fest</GlobalMenu>
+    <div class="flex flex-col items-center gap-3 max-w-6xl w-full px-10" style="margin-top: auto; margin-bottom: auto;">
+      <img src="@/assets/logo.png" class="w-full max-w-xl" alt="Logo" />
+      <div class="text-center">SUNForge is Sunway Unviersity's engineering project showcase, demonstrating our students' innovation and creativity in approaching complexx engineering problems.</div>
+      <div class="text-center">In semester 2025 December, we expect over 100 students from various engineering disciplines to present around 50 projects across multiple courses, including Project Management, Sustainable Engineering Design, Integrated Design Projects, and Final Year Projects. </div>
+    </div>
   </div>
 </template>
---
+++ src/components/GlobalMenu.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import { Button } from "@/components/ui/button";
+import { Menu } from 'lucide-vue-next';
+</script>
+
+<template>
+  <div class="absolute top-0 w-full p-2 text-xl bg-slate-200 text-slate-500 text-center flex flex-row items-center justify-center gap-2">
+    <Menu></Menu>
+    <div class="flex-1 grow">
+      <slot></slot>
+    </div>
+  </div>
+</template>
