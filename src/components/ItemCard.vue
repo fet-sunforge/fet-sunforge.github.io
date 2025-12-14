87871d8aeca4caf9847c42e0538dfaafe9195421
Richard Wong <ricwtk@gmail.com>
2025-12-14 23:33:58 +0800

    update project card display

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -8,16 +8,18 @@
     class="cursor-pointer"
     @click="$emit('open', item)"
   >
-    <Card class="rounded-xl shadow-sm border border-border p-4">
+    <Card class="rounded-xl shadow-sm border border-border p-4 h-[12em]">
       <CardHeader>
-        <CardTitle class="overflow-hidden h-[2.2em] line-clamp-2">{{ item.title }}</CardTitle>
+        <CardTitle class="line-clamp-2 overflow-hidden pb-[0.2em]">{{ item.title }}</CardTitle>
         <CardDescription class="flex flex-row gap-2 overflow-hidden">
           <UsersIcon v-if="group" />
           <UserIcon v-else />
           <div class="flex-1 text-nowrap overflow-hidden text-ellipsis">{{ item.pic.join(", ") }}</div>
         </CardDescription>
       </CardHeader>
-      <CardContent class="overflow-hidden text-ellipsis line-clamp-3 h-[4.5em]" v-html="item.desc.replaceAll('<br>', ' ')"></CardContent>
+      <CardContent class="flex-1 overflow-hidden flex flex-col justify-end">
+        <div class="line-clamp-3 h-[4.5em]" v-html="item.desc.replaceAll('<br>', ' ')"></div>
+      </CardContent>
     </Card>
   </div>
 </template>
