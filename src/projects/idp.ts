e4e2b7e2c834cc53da7d852d64bca67c7355aca4
Richard Wong <ricwtk@gmail.com>
2025-12-16 08:38:42 +0800

    update idp project description

--- src/projects/idp.ts
+++ src/projects/idp.ts
@@ -7,17 +7,21 @@ export const courses = ref([{
   description: "The Integrated Design Project is an engineering project carried out by a group of students to integrate knowledge and skills that has been gained. Working in groups of 3 to 4, students will be responsible for identifying and solving complex engineering problems and to carry out the necessary complex engineering activities which includes conceptualizing, designing, planning, implementing, managing and evaluating an Electronic and Electrical engineering design project. This will incorporate teamwork and leadership skills, effective communication and the application of project management.",
   short: "IDP",
   projects: [{
-    title: 'project 1',
-    desc: 'abstract 1',
-    pic: ['person1', 'person2']
+    title: 'Distributed Acoustic Sensing of Seismic Events in Buildings due to Earthquake',
+    desc: '',
+    pic: ['Group 1']
   }, {
-    title: 'project 2',
-    desc: 'abstract 2',
-    pic: ['person3']
+    title: 'Seismic Detection of Bridge due to Earthquake using Vibration Sensor and Accelerometer',
+    desc: '',
+    pic: ['Group 2']
   }, {
-    title: 'project 3',
-    desc: 'abstract 3',
-    pic: ['person5', 'person6', 'person7', 'person8']
+    title: 'Integrated Sensing of Landslide',
+    desc: '',
+    pic: ['Group 3']
+  }, {
+    title: 'Sinkhole Detection using Distributed Acoustic Sensing',
+    desc: '',
+    pic: ['Group 4']
   }]
 }, {
   name: "Integrated Design Project 2",
@@ -25,15 +29,15 @@ export const courses = ref([{
   short: "IDP",
   projects: [{
     title: 'project 1',
-    desc: 'abstract 1',
-    pic: ['person1', 'person2']
+    desc: '',
+    pic: ['Group 1']
   }, {
     title: 'project 2',
-    desc: 'abstract 2',
-    pic: ['person3']
+    desc: '',
+    pic: ['Group 2']
   }, {
     title: 'project 3',
-    desc: 'abstract 3',
-    pic: ['person5', 'person6', 'person7', 'person8']
+    desc: '',
+    pic: ['Group 3']
   }]
 }] as Array<Course>);
