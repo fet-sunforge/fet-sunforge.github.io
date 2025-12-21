7c91cad99d4d325f1ad07f3c6b65366ae3bafd59
Richard Wong <ricwtk@gmail.com>
2025-12-22 00:14:12 +0800

    update ebooklet page

--- src/pages/ebooklet/Ebooklet.vue
+++ src/pages/ebooklet/Ebooklet.vue
@@ -1,5 +1,7 @@
 <template>
-<iframe src="/ebooklet.pdf" width="100%" height="500vh">
+<div class="flex flex-col items-center justify-center h-screen w-full p-10">
+<iframe src="/ebooklet.pdf" width="100%" height="100%">
   This browser does not support inline PDFs. Please download the file to view it.
 </iframe>
+</div>
 </template>
