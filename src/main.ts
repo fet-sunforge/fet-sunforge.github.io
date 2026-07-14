1bc060ee0bf9cc50dbccc82a37f29b92315cb289
Richard Wong <ricwtk@gmail.com>
2026-07-14 15:40:32 +0800

    update font

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-import 'geist/dist/fonts/geist-sans/style.css' // Imports the Geist Sans font faces
-import 'geist/dist/fonts/geist-mono/style.css' // Imports the Geist Mono font faces
-
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
